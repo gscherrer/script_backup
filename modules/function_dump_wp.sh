875e84b9b606a56ef473f4d7e94981d011aa5e20
gscherrer <guillaume.scherrer29@gmail.com>
2025-01-29 11:19:39 +0100

    test de recup des id bdd

--- modules/function_dump_wp.sh
+++ modules/function_dump_wp.sh
@@ -46,9 +46,9 @@ function dump_wordpress_databases() {
   for INSTALLATION_DIR in "$ROOT_DIR"/*/; do
     if [ -f "$INSTALLATION_DIR/app/config/parameters.php" ]; then
       # Extract the database connection details from w/app/config/parameters.php
-      DATABASE=$(grep -oP "define\(\s*'database_name'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
-      DB_USER=$(grep -oP "define\(\s*'database_user'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
-      DB_PASSWORD=$(grep -oP "define\(\s*'database_password'\s*,\s*'\K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
+      DATABASE=$(grep -oP "\s*'database_name' =>'\K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
+      DB_USER=$(grep -oP "\s*'database_user' =>'\K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
+      DB_PASSWORD=$(grep -oP "\s*'database_password' => 'K[^']+" "$INSTALLATION_DIR/app/config/parameters.php")
       echo "Dumping database: $DATABASE"
       echo "User: $DB_USER"
       echo "Password: $DB_PASSWORD"
